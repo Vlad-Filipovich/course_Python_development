5a48d41c315daac755b80a859687d4d0841070a7
Vlad Filipovich <fvfordota2@gmail.com>
2022-11-22 23:26:34 +0300

    + one output in modules

--- first/modules.py
+++ first/modules.py
@@ -1,7 +1,9 @@
 import greetings
 from greetings import say_hi
+import goodbyes
 from goodbyes import say_hasta_la_vista_baby as terminator
 
 greetings.say_hello()
 say_hi()
+goodbyes.say_bye_bye()
 terminator()
